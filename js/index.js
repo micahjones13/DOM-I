390eec7849a35dbc77b6911ab4c62afe980014e7
micahjones13 <micahjones13@gmail.com>
2019-05-13 13:03:24 -0600

    finished matching, need to now add new content

--- js/index.js
+++ js/index.js
@@ -40,3 +40,50 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+//update img for header 
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"] )
+//image for middle content
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+//nav 
+
+// let nav = document.querySelector('nav');
+let navItem = document.querySelectorAll('nav a');
+navItem[0].textContent = siteContent.nav["nav-item-1"];
+navItem[1].textContent = siteContent.nav["nav-item-2"];
+navItem[2].textContent = siteContent.nav["nav-item-3"];
+navItem[3].textContent = siteContent.nav["nav-item-4"];
+navItem[4].textContent = siteContent.nav["nav-item-5"];
+navItem[5].textContent = siteContent.nav["nav-item-6"];
+//cta
+let ctaH1 = document.querySelector(".cta h1");
+ctaH1.textContent = siteContent.cta.h1;
+let btn = document.querySelector(".cta button");
+btn.textContent = siteContent.cta.button;
+
+//main content
+let paraContent = document.querySelectorAll('.text-content p');
+let paraH4 = document.querySelectorAll('.text-content h4');
+// let featuresTitle = document.querySelector('.main-content h4');
+// featuresTitle.textContent = siteContent["main-content"]["features-h4"];
+
+//paragraph h4's 
+paraH4[0].textContent = siteContent["main-content"]["features-h4"];
+paraH4[1].textContent = siteContent["main-content"]["about-h4"];
+paraH4[2].textContent = siteContent["main-content"]["services-h4"];
+paraH4[3].textContent = siteContent["main-content"]["product-h4"];
+paraH4[4].textContent = siteContent["main-content"]["vision-h4"];
+//paragraph content
+paraContent[0].textContent = siteContent["main-content"]["features-content"];
+paraContent[1].textContent = siteContent["main-content"]["about-content"];
+paraContent[2].textContent = siteContent["main-content"]["services-content"];
+paraContent[3].textContent = siteContent["main-content"]["product-content"];
+paraContent[4].textContent = siteContent["main-content"]["vision-content"];
+//contact
+let contactHeader = document.querySelector('.contact h4');
+contactHeader.textContent = siteContent.contact["contact-h4"];
+let contactAddress = document.querySelectorAll('.contact p');
+contactAddress[0].textContent = siteContent.contact.address;
+contactAddress[1].textContent = siteContent.contact.phone;
+contactAddress[2].textContent = siteContent.contact.email;
